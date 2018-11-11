adb4984139887d506255f0922d10f0a79315420c
Jakub Langowski <35259601+Mr1gowski@users.noreply.github.com>
2018-11-11 07:33:27 +0100

    Update config.php

--- librus/librus/config.php
+++ librus/librus/config.php
@@ -1,26 +1,27 @@
 <?php
-
+session_start();
+require_once "connect.php";
 $polaczenie =new mysqli($host,$user_name,$db_password,$db_name);
 
-if ($polaczenie->connect_errno!=0)
-{
-}
+if ($polaczenie->connect_errno!=0) {echo "Error: ".$polaczenie->connect_errno;}
 else
 {
-$login=$_POST['login'];
-$haslo=$_POST['haslo'];
-$sql_n="select * FROM uzytkownicy where login=''$login' AND haslo= '$haslo'AND typ='1'"
-$sql_u="select * FROM uzytkownicy where login=''$login' AND haslo= '$haslo'AND typ='0'"
-if ($result=$polaczenie->query($sql_n))
-{
+  $login=$_POST['login'];
+  $haslo=$_POST['haslo'];
+  $sql_n="select * FROM uzytkownicy where login=''$login' AND haslo= '$haslo'AND typ='1'"
+  $sql_u="select * FROM uzytkownicy where login=''$login' AND haslo= '$haslo'AND typ='0'"
+    if ($result=$polaczenie->query($sql_n))
+        {
 
-}
-else if($result=$polaczenie->query($sql_u))
-{
-
-
-}
-$polaczenie->close();
+          include_once 'strona_glowna_nauczyciel';
+          $result->close();
+        }
+      else if($result=$polaczenie->query($sql_u))
+        {
+          include_once 'strona_glowna_uczen';
+          $result->close();
+        }
+  $polaczenie->close();
 }
 
 
