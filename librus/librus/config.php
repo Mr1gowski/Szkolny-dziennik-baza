9c955cd888eb84277e2ed93eb40814e41220f289
Jakub Langowski <35259601+Mr1gowski@users.noreply.github.com>
2018-11-11 08:00:39 +0100

    Update config.php

--- librus/librus/config.php
+++ librus/librus/config.php
@@ -12,14 +12,21 @@ else
   $sql_u="select * FROM uzytkownicy where login=''$login' AND haslo= '$haslo'AND typ='0'"
     if ($result=$polaczenie->query($sql_n))
         {
-
+          $_SESSION['zalogowany']=true;
           include_once 'strona_glowna_nauczyciel';
+            unset($_SESSION['blad']);
           $result->close();
         }
       else if($result=$polaczenie->query($sql_u))
         {
+            $_SESSION['zalogowany1']=true;
           include_once 'strona_glowna_uczen';
+            unset($_SESSION['blad']);
           $result->close();
+        }
+        else {
+          $_SESSION['blad']='<span style"color:red">spell red</span>';
+          header('Location:index.php');
         }
   $polaczenie->close();
 }
